63ed58e33f5c3576e2820dfb9f966448ae124641
Jana Ismail <jana.ismail.nss@gmail.com>
2024-10-25 12:10:26 -0500

    Updated SurvivorLogs and SurvivorNotes viewsets to only handle logged in user's data

--- survivorapi/views/survivor_logs.py
+++ survivorapi/views/survivor_logs.py
@@ -19,6 +19,16 @@ class SurvivorLogs(viewsets.ModelViewSet):
     ViewSet for viewing and updating survivor logs in an active season log
     """
 
-    queryset = SurvivorLog.objects.all()
+    # queryset = SurvivorLog.objects.all()
     serializer_class = SurvivorLogSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        """Filters survivor logs based on user permissions"""
+        user = self.request.auth.user
+        if user.is_staff:
+            return SurvivorLog.objects.all()
+
+        return SurvivorLog.objects.filter(
+            user=user
+        )
--- survivorapi/views/survivor_notes.py
+++ survivorapi/views/survivor_notes.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, permissions
 from rest_framework.response import Response
 from rest_framework import serializers
 from rest_framework import status
-from survivorapi.models import SurvivorLog, Survivor, SurvivorNote
+from survivorapi.models import SurvivorLog, SurvivorNote
 
 class SurvivorNoteSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,18 +11,36 @@ class SurvivorNoteSerializer(serializers.ModelSerializer):
 
 class SurvivorNotes(viewsets.ModelViewSet):
     """
-    ViewSet for CRUD operations for notes about survivors in a season log
+    ViewSet for CRUD operations for survivor (log) notes in a season log
     """
 
-    queryset = SurvivorNote.objects.all()
+    # queryset = SurvivorNote.objects.all()
     serializer_class = SurvivorNoteSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        Filters notes based on user permissions
+        - Admin users can see all notes
+        - Regular users can only view their own notes
+        """
+        user = self.request.auth.user
+        if user.is_staff:
+            return SurvivorNote.objects.all()
+
+        return SurvivorNote.objects.filter(
+            survivor_log__user=user
+        )
 
     def create(self, request, *args, **kwargs):
         """Method for a user to create a note instance for a survivor in a season log"""
         try:
+            survivor_log = SurvivorLog.objects.get(
+                pk=request.data["survivor_log_id"],
+                user=request.user
+            )
             note = SurvivorNote.objects.create(
-                survivor_log_id = request.data["survivor_log_id"],
+                survivor_log_id = survivor_log.id,
                 text = request.data["text"]
             )
 
